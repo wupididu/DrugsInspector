beaee6f142d8f16cfd4faa8d668768db431feef6
Ilya <ilyasmirkin200137@gmail.com>
2021-02-13 13:31:01 -0800

    Added server
    reorganized project tree

---
+++ Server/srv.py
@@ -0,0 +1,24 @@
+import jpysocket
+import socket
+import codecs
+soc = jpysocket.jpysocket()
+host = ""
+port = 1999
+soc.bind(('', port))       # Bind to the port
+soc.listen(5)
+try:
+	while True:
+		conn, addr = soc.accept()     # Establish connection with client.
+		print ("Got connection from",addr)
+		msg = conn.recv(1024)
+		msg = codecs.decode(msg, 'utf-8')
+		print (msg)
+		if (msg == "Hello"):
+			conn.send(jpysocket.jpyencode("facepalm chel ty v mute"))
+			print("norm chel")
+		else:
+			conn.send(jpysocket.jpyencode("ladno)"))
+			print("stupid fuck")
+	soc.close()
+except KeyboardInterrupt:
+	exit(0)
