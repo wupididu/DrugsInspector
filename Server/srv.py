016991bc1f63971b351433aa6209d14db337b89c
Ilya <ilyasmirkin200137@gmail.com>
2021-02-14 14:54:21 -0800

    Opened project in PyCharm
    re-formated code and removed unnececarry code

--- Server/srv.py
+++ Server/srv.py
@@ -1,24 +1,23 @@
 import jpysocket
-import socket
 import codecs
+
 soc = jpysocket.jpysocket()
 host = ""
 port = 1999
-soc.bind(('', port))       # Bind to the port
+soc.bind(('', port))  # Bind to the port
 soc.listen(5)
 try:
-	while True:
-		conn, addr = soc.accept()     # Establish connection with client.
-		print ("Got connection from",addr)
-		msg = conn.recv(1024)
-		msg = codecs.decode(msg, 'utf-8')
-		print (msg)
-		if (msg == "Hello"):
-			conn.send(jpysocket.jpyencode("facepalm chel ty v mute"))
-			print("norm chel")
-		else:
-			conn.send(jpysocket.jpyencode("ladno)"))
-			print("stupid fuck")
-	soc.close()
+    while True:
+        conn, addr = soc.accept()  # Establish connection with client.
+        print("Got connection from", addr)
+        msg = conn.recv(1024)
+        msg = codecs.decode(msg, 'utf-8')
+        print(msg)
+        if (msg == "Hello"):
+            conn.send(jpysocket.jpyencode("facepalm chel ty v mute"))
+            print("norm chel")
+        else:
+            conn.send(jpysocket.jpyencode("ladno)"))
+            print("stupid fuck")
 except KeyboardInterrupt:
-	exit(0)
+    exit(0)
